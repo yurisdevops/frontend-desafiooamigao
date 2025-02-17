2c7dfb1222968f16df379094aaa93ef3849e8b66
Yuri Souza <143803323+yurisdevops@users.noreply.github.com>
2025-02-17 18:38:47 -0300

    Atualizar o main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")).render(
         autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
-        closeOnClick={false}
+        closeOnClick={true}
         rtl={false}
         pauseOnFocusLoss
         draggable
